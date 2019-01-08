6bc6cd21abd88c0787f5d0b4c89a8d7e69204fdd
Eyal Toledano <eyaltoledano@me.com>
2019-01-08 03:44:09 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,7 +2,13 @@ def using_until
   levitation_force = 6
 
   until levitation_force > 10 
+
     puts "Wingardium Leviosa"
     levitation_force =+ 1
+
+    if levitation_force = 10
+      break
+    end
+    
   end
 end
